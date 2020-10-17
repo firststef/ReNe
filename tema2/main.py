2f6bb178a71babffad6a02ccca263d1b18c4418d
firststef <firstbossspike@gmail.com>
2020-10-17 14:37:15 +0300

    Added perceptron algorithms

---
+++ tema2/main.py
@@ -0,0 +1,12 @@
+import gzip
+import pickle
+import perceptron
+
+with gzip.open('mnist.pkl.gz', 'rb') as f:
+    train_set, valid_set, test_set = pickle.load(f, encoding='latin')
+
+    zero_set = [(x, 0 if t != 0 else 1) for x, t in zip(train_set[0], train_set[1])]
+    weights, b = perceptron.adeline_perceptron(zero_set, len(train_set[0][0]))
+
+    print(perceptron.compute_result_for_perceptron(weights, b, test_set[0][0]))
+    print(perceptron.compute_result_for_perceptron(weights, b, test_set[0][1]))
---
+++ tema2/perceptron.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+
+def perceptron_activation(ii: int):
+    return 1 if ii > 0 else 0
+
+
+def perceptron_online_training(training_set, num_of_features):
+    weights = np.array([0 for x in range(num_of_features)])
+    all_classified = False
+    nr_iterations = 1000000
+    b = 0
+    miu = 0.01
+
+    while not all_classified and nr_iterations > 0:
+        all_classified = True
+        for i, (x, t) in enumerate(training_set):
+            z = np.dot(weights, x) + b
+            output = perceptron_activation(z)
+            weights += (t - output) * x * miu
+            b += (t - output) * miu
+            if output != t:
+                all_classified = True
+        nr_iterations -= 1
+    return weights, b
+
+
+def perceptron_batch_training(training_set, num_of_features):
+    weights = np.array([0 for x in range(num_of_features)])
+    nr_iterations = 1000000
+    b = 0
+    miu = 0.01
+    batches = len(training_set) // 100
+
+    while nr_iterations > 0:
+        alfas = np.array([0 for x in range(batches)])
+        betas = np.array([0 for x in range(batches)])
+        for ba in range(batches):
+            alfas[ba] = np.array([0 for x in range(num_of_features)])
+            betas[ba] = 0
+            for v, (x, t) in enumerate(training_set[ba * len(training_set) / batches:(ba + 1) * len(training_set) / batches]):
+                z = np.dot(weights, x) + b
+                output = perceptron_activation(z)
+                alfas[ba] += (t - output) * x * miu
+                betas[ba] += (t - output) * miu
+
+        for ba in range(batches):
+            weights += alfas[ba]
+            b += betas[ba]
+        nr_iterations -= 1
+    return weights, b
+
+
+def perceptron_mini_batch_training(training_set, num_of_features):
+    weights = np.array([0 for x in range(num_of_features)])
+    nr_iterations = 1000000
+    b = 0
+    miu = 0.01
+    batches = len(training_set) // 100
+
+    while nr_iterations > 0:
+        for ba in range(batches):
+            alfa = np.array([0 for x in range(num_of_features)])
+            beta = 0
+            for v, (x, t) in enumerate(
+                    training_set[ba * len(training_set) / batches:(ba + 1) * len(training_set) / batches]):
+                z = np.dot(weights, x) + b
+                output = perceptron_activation(z)
+                alfa += (t - output) * x * miu
+                beta += (t - output) * miu
+            weights += alfa
+            b += beta
+        nr_iterations -= 1
+    return weights, b
+
+
+def adeline_perceptron(training_set, num_of_features):
+    weights = np.array([0.0 for x in range(num_of_features)])
+    nr_iterations = 1000000
+    b = 0
+    miu = 0.01
+
+    while nr_iterations > 0:
+        for i, (x, t) in enumerate(training_set):
+            z = np.dot(weights, x) + b
+            weights += (float(t) - z) * x * miu
+            b += (t - z) * miu
+        nr_iterations -= 1
+    return weights, b
+
+
+def compute_result_for_perceptron(weights, b, input_f):
+    z = np.dot(weights, input_f) + b
+    print(z)
+    return perceptron_activation(z)
